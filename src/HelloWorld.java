03b5830a680ee8337fae17f68e067089a596eedd
unknown <wven2@avans.nl>
2015-04-24 11:54:39 +0200

    My firs commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		int sneeze = 10;
+		 int counter = 0;
+		 
+		 while (counter < sneeze){
+		 System.out.print("Ha");
+		 counter = counter + 1;
+		 }
+System.out.print("choo Hello Wouter");
+		}
+}
